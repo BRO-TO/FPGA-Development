0c140d58c482e33120b01a8271cc9afac252e290
BRO-TO <82194622+BRO-TO@users.noreply.github.com>
2022-01-13 16:07:26 +0700

    Solution for Tb_tb2

---
+++ VERILOG/HDLBits_Solution/5. Verification : Writing Testbenches/4. Testbench2.v
@@ -0,0 +1,30 @@
+module top_module();
+    reg clk;
+    reg in;
+    reg [2:0] s;
+    wire out;
+    
+    q7 dut(clk,in,s,out);
+    
+    initial begin
+        #0 clk = 0;
+        forever
+            #5 clk = ~clk;
+    end
+    
+    initial begin
+        #0  in = 0;
+        #20 in = 1;
+        #10 in = 0;
+        #10 in = 1;
+        #30 in = 0;
+    end
+    
+    initial begin
+    	  #0  s = 3'd2;
+        #10 s = 3'd6;
+        #10 s = 3'd2;
+        #10 s = 3'd7;
+        #10 s = 3'd0;
+    end
+endmodule
