50af7dde4573a4b22c68009e9442569ba5c41486
BRO-TO <82194622+BRO-TO@users.noreply.github.com>
2022-01-13 16:09:25 +0700

    Update 3. AND gate.v

--- VERILOG/HDLBits_Solution/5. Verification : Writing Testbenches/3. AND gate.v
+++ VERILOG/HDLBits_Solution/5. Verification : Writing Testbenches/3. AND gate.v
@@ -2,7 +2,8 @@ module top_module ( output reg A, output reg B );
 
     // generate input patterns here
     initial begin
-		A = 1'b0;
+	    
+	A = 1'b0;
         B = 1'b0;
         #10;
         A = 1'b1;
@@ -12,6 +13,7 @@ module top_module ( output reg A, output reg B );
         A = 1'b0;
         #20
         B = 1'b0; 
+	    
     end
 
 endmodule
