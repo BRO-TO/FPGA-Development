d2d3e0f1ce5925cb3f518f9fec8c37cbda272523
BRO-TO <82194622+BRO-TO@users.noreply.github.com>
2022-01-13 16:08:17 +0700

    Solution for Tb_tff

---
+++ VERILOG/HDLBits_Solution/5. Verification : Writing Testbenches/5. T flip-flop.v
@@ -0,0 +1,33 @@
+module top_module ();
+    
+    reg clk;
+    reg reset;
+    reg t;
+    wire q;
+    
+    tff DUT(clk,reset,t,q);
+    
+    initial begin 
+        clk = 0;
+        reset = 0;
+        t = 0;
+        #20
+        reset = 1;
+        #20
+        reset = 0;   
+    end
+    
+    always begin
+        #5 clk = ~clk;
+    end
+    
+    always @(posedge clk) begin
+        if (reset) begin
+            t = 0;
+        end
+        else begin
+ 			      t = 1;
+        end
+    end
+
+endmodule
